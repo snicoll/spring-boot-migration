b90bc849dd23f79e09d949366cb4f4c40404f671
Stephane Nicoll <snicoll@pivotal.io>
2018-04-05 13:53:07 +0200

    Upgrade to Spring Boot 2
    When we upgrade our sample app to Spring Boot 2, several dependencies
    will be upgraded as well.
    
    The webjars locator feature was using the wrong dependency in Spring
    Boot 1 so we've taken the opportunity in Spring Boot 2 to fix that. We
    no longer provide dependency management for the former dependency so
    that you notice it on upgrade.
    
    Also, Spring Boot 2 comes with Spring Framework 5 and a Java 8 base
    so we can benefit from new features such as default methods in interfaces
    that makes the Adapter layer unnecessary.

--- src/main/java/com/example/speakerservice/SecurityConfig.java
+++ src/main/java/com/example/speakerservice/SecurityConfig.java
@@ -1,16 +1,13 @@
 package com.example.speakerservice;
 
-import org.springframework.boot.autoconfigure.security.SecurityProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.annotation.Order;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 @Configuration
-@Order(SecurityProperties.ACCESS_OVERRIDE_ORDER)
 class SecurityConfig extends WebSecurityConfigurerAdapter {
 
 	@Override
